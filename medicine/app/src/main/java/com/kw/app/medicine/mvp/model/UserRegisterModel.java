1d38f33ff6700df3b099edc603683d5ec50a89c8
tingyouwu <wutingyou@wxchina.com>
2016-10-13 16:27:06 +0800

    注册页面去掉压缩图片

--- medicine/app/src/main/java/com/kw/app/medicine/mvp/model/UserRegisterModel.java
+++ medicine/app/src/main/java/com/kw/app/medicine/mvp/model/UserRegisterModel.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.text.TextUtils;
 
 import com.kw.app.commonlib.utils.FileUtils;
-import com.kw.app.commonlib.utils.luban.Luban;
-import com.kw.app.commonlib.utils.luban.OnCompressListener;
 import com.kw.app.medicine.base.BmobFileManager;
 import com.kw.app.medicine.base.BmobUserManager;
 import com.kw.app.medicine.base.IFileManager;
@@ -14,8 +12,6 @@ import com.kw.app.medicine.data.local.UserDALEx;
 import com.kw.app.medicine.mvp.contract.IUserRegisterContract;
 import com.kw.app.ormlib.OrmModuleManager;
 import com.kw.app.widget.ICallBack;
-
-import java.io.File;
 
 /**
  * @author wty
@@ -28,25 +24,21 @@ public class UserRegisterModel implements IUserRegisterContract.IUserRegisterMod
     @Override
     public void register(final Context context, final UserDALEx user, final ICallBack<String> callBack) {
         if(!TextUtils.isEmpty(user.getLogourl())){
-            //压缩图片
-            Luban.get().load(new File(user.getLogourl()))
-                       .putGear(Luban.THIRD_GEAR)
-                       .setCompressListener(new OnCompressListener() {
-                            @Override
-                            public void onStart() {
-                            }
+            uploadFile(context, user.getLogourl(), new ICallBack<String>() {
+                @Override
+                public void onSuccess(String url) {
+                    user.setLogourl(url);
+                    //上传完头像后 删掉本地截图文件
+                    FileUtils.deleteFile(user.getLogourl());
+                    signUp(user,callBack);
+                }
 
-                            @Override
-                            public void onSuccess(File file) {
-                                // 压缩完毕就上传头像图片
-                                uploadFile(context,file.getAbsolutePath(), user, callBack);
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-                                callBack.onFaild("压缩图片失败:" + e.getMessage());
-                            }
-                        }).launch();
+                @Override
+                public void onFaild(String msg) {
+                    FileUtils.deleteFile(user.getLogourl());
+                    callBack.onFaild(msg);
+                }
+            });
         }else{
             signUp(user,callBack);
         }
@@ -55,21 +47,17 @@ public class UserRegisterModel implements IUserRegisterContract.IUserRegisterMod
     /**
      * @Decription 上传文件
      **/
-    private void uploadFile(final Context context, final String compresspath, final UserDALEx data, final ICallBack<String> callBack){
+    private void uploadFile(final Context context, final String compresspath, final ICallBack<String> callBack){
 
         fileManager = BmobFileManager.getInstance();
         fileManager.uploadFile(context, compresspath, new ICallBack<String>() {
             @Override
             public void onSuccess(String url) {
-                data.setLogourl(url);
-                //上传完头像后 删掉本地截图文件
-                FileUtils.deleteFile(compresspath);
-                signUp(data,callBack);
+                callBack.onSuccess(url);
             }
 
             @Override
             public void onFaild(String msg) {
-                FileUtils.deleteFile(compresspath);
                 callBack.onFaild(msg);
             }
         });
